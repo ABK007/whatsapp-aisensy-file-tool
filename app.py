74ea113b1299a80b2991ab84830acb4d2750dc3e
Muhammad Abubakar Khalid <abubakarzzz07@gmail.com>
2025-04-10 12:13:41 -0700

    added functions to remove duplicates and add new columns and enter file name functionalites

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from st_components import st_select_multiple_files
-from functions import add_plus_to_phone
+from functions import add_plus_to_phone, remove_blank_rows
 
 st.title("Creating CSV file for WhatsApp Business API")
 
@@ -11,12 +11,19 @@ df = st_select_multiple_files()
 
 st.markdown("---") # Add a separator line
 
-if df is not None:
+if df is None:
+    st.warning("Select at least one .csv file to upload")
+
+else:
     st.write(f"Total rows in combined CSV: {df.shape[0]}")
     st.write(f"Total columns in combined CSV: {df.shape[1]}")
     st.write("### Combined CSV files preview")
+    
+    df = remove_blank_rows(df, column_name="phoneUnformatted") # removing blank rows from the specified column
+    df = df.drop_duplicates() # removing duplicates
     df = add_plus_to_phone(df, column_name="phoneUnformatted") # converting the phone column to start with '+'
-    st.dataframe(df.head())
+    
+    st.dataframe(df.head(n=20))
 
     st.write("### Output CSV Column Selection")
 
@@ -25,7 +32,7 @@ if df is not None:
     selected_columns = st.multiselect(
         "Select columns to include:",
         options=df.columns.tolist(),
-        default=df.columns.tolist()
+        default=["title", "phoneUnformatted", "city"]
     )
 
     # For each selected column, allow the user to input a desired output name.
@@ -36,11 +43,20 @@ if df is not None:
         mapping[col] = new_name
 
     # Show a preview of the DataFrame with renamed columns.
+    
+    st.write("### Adding Tags column")
+    tag_value = st.text_input("Tag Column Value")
     if selected_columns:
         # Rename only the selected columns, leave the others unchanged if they are not selected.
         df_subset = df[selected_columns].rename(columns=mapping)
+        
+
+        
+        df_subset["Tags"] = tag_value # Adding new Tag column with value from the input field
         st.write("### Preview of Mapped DataFrame")
         st.dataframe(df_subset)
+        
+        file_name = st.text_input("Enter file name to generate .csv file", value="mapped_output.csv")
 
         # Button to generate CSV
         if st.button("Generate CSV"):
@@ -48,7 +64,7 @@ if df is not None:
             st.download_button(
                 label="Download CSV",
                 data=csv_data,
-                file_name="mapped_output.csv",
+                file_name=f"{file_name}",
                 mime="text/csv"
             )
     else:
--- functions.py
+++ functions.py
@@ -20,3 +20,21 @@ def add_plus_to_phone(df, column_name="phoneUnformatted"):
     else:
         raise KeyError(f"Column '{column_name}' not found in the DataFrame.")
     return df
+
+
+def remove_blank_rows(df, column_name="phoneUnformatted"):
+    """
+    Remove the rows in dataframe with empty values or empty strings
+    
+    Parameters:
+        df (pd.DataFrame): The input DataFrame.
+        column_name (str): The name of the phone column to convert (default "phone_unformatted").
+        
+    Returns:
+        dataframe with removed empty values rom the specified column.
+        
+    """
+    df_clean = df.dropna(subset=[column_name])    # Remove NaN values first
+    df_clean = df_clean[df_clean[column_name] != ''] # Remove empty strings next
+    
+    return df_clean
