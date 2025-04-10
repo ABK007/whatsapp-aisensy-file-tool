e564a5a7c180b5457399f3584fa8ff48a3c4b5ff
Muhammad Abubakar Khalid <abubakarzzz07@gmail.com>
2025-04-10 00:52:41 -0700

    created a combined dataframe preview

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 import streamlit as st
 import pandas as pd
+from st_components import st_select_multiple_files
 
 st.title("File Upload Example")
 
-
-# Create a file uploader widget that accepts CSV files.
-uploaded_files = st.file_uploader(
-    "Choose a CSV file", type=["csv"], accept_multiple_files=True
-)
-
-if uploaded_files:
-    st.write(f"Total files uploaded: {len(uploaded_files)}")
-    for uploaded_file in uploaded_files:
-        st.write(f"Filename: {uploaded_file.name}")
-        try:
-            # Process each file; assuming it's a CSV file
-            df = pd.read_csv(uploaded_file)
-            st.write(f"Preview of {uploaded_file.name}:")
-            st.dataframe(df.head())
-        except Exception as e:
-            st.error(f"Error reading {uploaded_file.name}: {e}")
+# This function will create a file uploader widget that accepts 
+# multiple CSV files and previews them and returns combined dataframe.
+df = st_select_multiple_files() 
+st.write("Combined CSV files preview")
+st.dataframe(df.head()) 
---
+++ st_components.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+
+def st_select_multiple_files():
+    """ Create a file uploader widget that accepts multiple CSV files and preview them. 
+    Also returns the combined dataframe of all uploaded files."""
+    uploaded_files = st.file_uploader(
+        "Choose a CSV file", type=["csv"], accept_multiple_files=True
+    )
+    
+    df_list = []  # List to hold dataframes for each uploaded file
+    combined_df = pd.DataFrame()  # Initialize an empty dataframe for combined data
+
+    if uploaded_files:
+        st.write(f"Total files uploaded: {len(uploaded_files)}")
+        for uploaded_file in uploaded_files:
+            st.write(f"Filename: {uploaded_file.name}")
+            
+            try:
+                # Process each file; assuming it's a CSV file
+                df = pd.read_csv(uploaded_file)
+                st.write(f"Preview of {uploaded_file.name}:")
+                st.dataframe(df.head())
+                
+                # add and combine the dataframes to the list
+                df_list.append(df)
+                
+                combined_df = pd.concat(df_list, ignore_index=True)
+                
+            except Exception as e:
+                st.error(f"following error occurred: {e}")
+                
+    return combined_df
+                
